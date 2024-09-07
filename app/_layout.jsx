11294a6a30a05f58aecfdcfb3bf9dcc6a45c3dcf
Jan Ong <janong24@gmail.com>
2024-09-06 23:00:44 -0400

    Iconography and Typography

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { Slot } from 'expo-router'
+import { useFonts } from 'expo-font'
+
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Manrope-ExtraBold": require("../assets/fonts/Manrope-ExtraBold.ttf"),
+    "Manrope-Bold": require("../assets/fonts/Manrope-Bold.ttf"),
+    "Manrope-SemiBold": require("../assets/fonts/Manrope-SemiBold.ttf"),
+    "Manrope-Medium": require("../assets/fonts/Manrope-Medium.ttf"),
+    "Manrope-Regular": require("../assets/fonts/Manrope-Regular.ttf"),
+    "Manrope-Light": require("../assets/fonts/Manrope-Light.ttf"),
+    "Manrope-Extralight": require("../assets/fonts/Manrope-Extralight.ttf"),
+  })
+
   return (
     <>
       <Text>Header</Text>
---
+++ constans/icons.jsx
@@ -0,0 +1,22 @@
+import { ChevronDoubleRightIcon as right } from "@heroicons/react/24/outline";
+import { ChevronDoubleLeftIcon as left } from "@heroicons/react/24/outline";
+import { HomeIcon as home } from "@heroicons/react/24/outline";
+import { HandThumbDownIcon as thumbdown } from "@heroicons/react/24/outline";
+import { HandThumbUpIcon as thumbup } from "@heroicons/react/24/outline";
+import { InformationCircleIcon as info } from "@heroicons/react/24/outline";
+import { Bars3Icon as menu } from "@heroicons/react/24/outline";
+import { WrenchScrewdriverIcon as settings } from "@heroicons/react/24/outline";
+import { XMarkIcon as close } from "@heroicons/react/24/outline";
+
+
+export default {
+  right,
+  left,
+  home,
+  thumbdown,
+  thumbup,
+  info,
+  menu,
+  settings,
+  close,
+};
